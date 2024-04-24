b3e92a98b658bb0949dcfb61448a9aab567e636c
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-04-24 13:41:25 +0200

    agent image editing

--- app/page.js
+++ app/page.js
@@ -550,6 +550,8 @@ export default function Home() {
             className="absolute -mt-6 ml-5"
             src={"/Assets/Images/agent.png"}
             alt=""
+            width={600}
+            height={600}
           />
           <div className="text-white mx-1 w-[500px] ml-[400px] mt-[70px]">
             <h1 className="text-[30px] font-semibold word">Become an Agent.</h1>
